9dd62d2c00caf9d32d65b1ddad90a0a7d536fe1b
bulumutka <mishasdk@gmail.com>
2020-06-04 19:59:46 +0300

    Copy button for formulas added

--- src/java/com/bulumutka/polyconstr/controllers/ShowResultController.java
+++ src/java/com/bulumutka/polyconstr/controllers/ShowResultController.java
@@ -2,16 +2,27 @@ package com.bulumutka.polyconstr.controllers;
 
 import com.bulumutka.polyconstr.models.Repository;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ShowResultController implements Initializable {
     public TextField formulasField;
+    public Button copyButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         formulasField.setText(Repository.formulas);
+
+        copyButton.setOnAction(e -> {
+            System.out.println("Copy button clicked");
+            var content = new ClipboardContent();
+            content.putString(formulasField.getText());
+            Clipboard.getSystemClipboard().setContent(content);
+        });
     }
 }
